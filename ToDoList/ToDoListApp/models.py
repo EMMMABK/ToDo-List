39775b562fdd89d34cb30d71ab73bb94a8b87a73
EMMMABK <adilbikiev2@gmail.com>
2025-01-08 21:54:36 +0600

    add Fix Bugs

--- ToDoList/ToDoListApp/models.py
+++ ToDoList/ToDoListApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-class Task:
+class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
@@ -13,4 +13,4 @@ class Task:
         return self.title
     
     class Meta:
-        ordering = ['Complete']
+        ordering = ['complete']
